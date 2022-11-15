ae37aa9b135af6f2ad7f7e2fcc6aa2ad0eca0d67
Pietro Ferrara <pietro.ferrara@unive.it>
2022-11-15 11:16:31 +0100

    adding mover plugin based on the Spring plugin library

--- DroneSystem/src/main/java/it/unive/dais/sa/dronesystem/DeliveryManager.java
+++ DroneSystem/src/main/java/it/unive/dais/sa/dronesystem/DeliveryManager.java
@@ -1,5 +1,6 @@
 package it.unive.dais.sa.dronesystem;
 
+import it.unive.dais.sa.dronesystem.droneMovers.MoverPluginRegistry;
 import org.apache.camel.CamelContext;
 import org.apache.camel.Exchange;
 import org.apache.camel.builder.RouteBuilder;
@@ -48,25 +49,17 @@ public class DeliveryManager {
         private void progress(Exchange exchange) {
             Position destination = delivery.getDestination();
             Position actualPosition = delivery.getDrone().getPosition();
-            Position new_position = approach(actualPosition, destination);
-            delivery.getDrone().setPosition(new_position);
-            drones.save(delivery.getDrone());
+            Drone drone = delivery.getDrone();
+            Position new_position =MoverPluginRegistry.registry.getPluginFor(drone).get()
+                    .approach(actualPosition, destination, delivery.getDrone());
+            drone.setPosition(new_position);
+            drones.save(drone);
             if(destination.equals(new_position)) {
                 completedDeliveries.save(new CompletedDelivery(delivery));
                 deliveries.deleteById(delivery.getId());
                 delivery = null;
                 ctx.stop();
             }
-        }
-
-        private Position approach(Position from, Position to) {
-            int x = from.getX() < to.getX() ?
-                    Math.min(to.getX(), from.getX()+1) :
-                    Math.max(to.getX(), from.getX()-1);
-            int y = from.getY() < to.getY() ?
-                    Math.min(to.getY(), from.getY()+1) :
-                    Math.max(to.getY(), from.getY()-1);
-            return new Position(x, y);
         }
     }
 }
